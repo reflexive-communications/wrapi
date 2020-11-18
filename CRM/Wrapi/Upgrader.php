d591886f4379c04688892ec36da1f8e4b7168321
Sandor Semsey <semseysandor@gmail.com>
2020-11-18 20:58:12 +0100

    settings

--- CRM/Wrapi/Upgrader.php
+++ CRM/Wrapi/Upgrader.php
@@ -10,7 +10,7 @@ class CRM_Wrapi_Upgrader extends CRM_Wrapi_Upgrader_Base
     /**
      * Extension prefix
      */
-    public const EXTENSION_PREFIX = 'extension'.E::SHORT_NAME;
+    public const EXTENSION_PREFIX = 'extension_'.E::SHORT_NAME;
 
     /**
      * Default settings
