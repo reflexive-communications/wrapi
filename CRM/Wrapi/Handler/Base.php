12546b9095d9ecd71f512efd4a76feb541397437
Sandor Semsey <semseysandor@gmail.com>
2020-11-24 16:20:49 +0100

    Base Handler

---
+++ CRM/Wrapi/Handler/Base.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Base Handler
+ *
+ * @package  wrapi
+ * @author   Sandor Semsey <sandor@es-progress.hu>
+ * @license  AGPL-3.0
+ */
+abstract class CRM_Wrapi_Handler_Base
+{
+    /**
+     * IO Processor
+     *
+     * @var CRM_Wrapi_Processor_Base
+     */
+    protected CRM_Wrapi_Processor_Base $processor;
+
+    /**
+     * CRM_Wrapi_Handler_Base constructor.
+     *
+     * @param CRM_Wrapi_Processor_Base $processor
+     */
+    public function __construct(CRM_Wrapi_Processor_Base $processor)
+    {
+        $this->processor = $processor;
+    }
+
+    /**
+     * Handle request
+     *
+     * @param $request_data
+     *
+     * @return mixed
+     */
+    abstract public function handle($request_data);
+}
