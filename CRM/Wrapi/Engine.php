e4bebd5c0a4e7fb3209f50857cfd9101e0991418
Sandor Semsey <semseysandor@gmail.com>
2020-11-24 15:59:33 +0100

    engine

--- CRM/Wrapi/Engine.php
+++ CRM/Wrapi/Engine.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * WrAPI Engine
+ * Engine
  *
  * @package  wrapi
  * @author   Sandor Semsey <sandor@es-progress.hu>
@@ -10,32 +10,11 @@
 class CRM_Wrapi_Engine
 {
     /**
-     * Request data
-     *
-     * @var array|string|null
-     */
-    protected $requestData;
-
-    /**
      * IO Processor
      *
      * @var CRM_Wrapi_Processor_Base|null $processor
      */
     protected ?CRM_Wrapi_Processor_Base $processor;
-
-    /**
-     * Authenticator
-     *
-     * @var CRM_Wrapi_Authenticator|null
-     */
-    protected ?CRM_Wrapi_Authenticator $authenticator;
-
-    /**
-     * Router
-     *
-     * @var CRM_Wrapi_Router|null
-     */
-    protected ?CRM_Wrapi_Router $router;
 
     /**
      * CRM_Wrapi_Engine constructor
@@ -55,19 +34,21 @@ class CRM_Wrapi_Engine
     {
         try {
             // Detect content-type & process input
-            $this->createProcessor(CRM_Wrapi_Processor_Base::detectContentType());
-            $this->requestData = $this->processor->processInput();
+            $this->processor = $this->createProcessor(CRM_Wrapi_Processor_Base::detectContentType());
+            $request_data = $this->processor->processInput();
 
             // Request now parsed --> authenticate
-            $this->createAuthenticator($this->processor);
-            $this->authenticator->authenticate($this->requestData['site_key'], $this->requestData['user_key']);
+            $authenticator = $this->createAuthenticator($this->processor);
+            $authenticator->authenticate($request_data['site_key'], $request_data['user_key']);
 
             // Civi bootstrapped --> route request
-            $this->createRouter($this->processor);
-            $handler=$this->router->route($this->requestData['action']);
+            $router = $this->createRouter($this->processor);
+            $handler_class = $router->route($request_data['action']);
 
+            // Handler found --> create handler & pass request to handler
+            $handler = $this->createHandler($handler_class);
+            $handler->handle($request_data);
 
-            $this->processor->output($this->requestData);
         } catch (CRM_Core_Exception $ex) {
             http_response_code(500);
             $this->processor->error((string)$ex, true);
@@ -81,24 +62,50 @@ class CRM_Wrapi_Engine
      * Create Processor
      *
      * @param string $processor_class Processor class name
+     *
+     * @return CRM_Wrapi_Processor_Base
      */
-    protected function createProcessor(string $processor_class): void
+    protected function createProcessor(string $processor_class): CRM_Wrapi_Processor_Base
     {
-        $this->processor = new $processor_class();
+        return new $processor_class();
     }
 
     /**
      * Create Authenticator
      *
      * @param CRM_Wrapi_Processor_Base $processor
+     *
+     * @return CRM_Wrapi_Authenticator
      */
-    protected function createAuthenticator(CRM_Wrapi_Processor_Base $processor): void
+    protected function createAuthenticator(CRM_Wrapi_Processor_Base $processor): CRM_Wrapi_Authenticator
     {
-        $this->authenticator = new CRM_Wrapi_Authenticator($processor);
+        return new CRM_Wrapi_Authenticator($processor);
     }
 
-    protected function createRouter(CRM_Wrapi_Processor_Base $processor):void
+    /**
+     * Create Router
+     *
+     * @param CRM_Wrapi_Processor_Base $processor
+     *
+     * @return CRM_Wrapi_Router
+     */
+    protected function createRouter(CRM_Wrapi_Processor_Base $processor): CRM_Wrapi_Router
     {
-        $this->router=new CRM_Wrapi_Router($processor);
+        return new CRM_Wrapi_Router($processor);
+    }
+
+    protected function createHandler(string $handler_class): CRM_Wrapi_Handler_Base
+    {
+        $handler = null;
+        if (!class_exists($handler_class)) {
+            $this->processor->error(sprintf('Handler: %s not exists', $handler_class));
+        }
+        $handler = new $handler_class($this->processor);
+
+        if (!($handler instanceof CRM_Wrapi_Handler_Base)) {
+            $this->processor->error(sprintf('Not handler: %s', $handler_class));
+        }
+
+        return $handler;
     }
 }
