6b5f8c61c06bebdd4cd18bfcf677ca4e3d99c21f
Sandor Semsey <semseysandor@gmail.com>
2021-02-17 21:14:06 +0100

    get actions

--- CRM/Wrapi/Actions/Get.php
+++ CRM/Wrapi/Actions/Get.php
@@ -3,6 +3,8 @@
 use Civi\API\Exception\UnauthorizedException;
 use Civi\Api4\Contact;
 use Civi\Api4\Email;
+use Civi\Api4\Phone;
+use Civi\Api4\Relationship;
 
 /**
  * Common Get Actions
@@ -16,7 +18,7 @@ use Civi\Api4\Email;
 class CRM_Wrapi_Actions_Get
 {
     /**
-     * Get contact ID by email
+     * Get contact ID from email
      *
      * @param string $email Email address
      * @param bool $check_permissions Should we check permissions (ACLs)?
@@ -25,21 +27,38 @@ class CRM_Wrapi_Actions_Get
      *
      * @throws API_Exception
      * @throws UnauthorizedException
-     * @throws CRM_Core_Exception
      */
     public static function contactIDFromEmail(string $email, bool $check_permissions = false): ?int
     {
-        CRM_Wrapi_Processor_Base::validateInput($email, 'string', 'Email address');
-
-        // Search DB
         $results = Email::get($check_permissions)
             ->addSelect('contact_id')
             ->addWhere('email', 'LIKE', $email)
             ->setLimit(1)
             ->execute();
 
-        // Return contact ID
         return $results->first()['contact_id'];
+    }
+
+    /**
+     * Get contact ID from external ID
+     *
+     * @param string $external_id External ID
+     * @param bool $check_permissions Should we check permissions (ACLs)?
+     *
+     * @return int|null Contact ID if found, null if not found
+     *
+     * @throws API_Exception
+     * @throws UnauthorizedException
+     */
+    public static function contactIDFromExternalID(string $external_id, bool $check_permissions = false): ?int
+    {
+        $results = Contact::get($check_permissions)
+            ->addSelect('id')
+            ->addWhere('external_identifier', '=', $external_id)
+            ->setLimit(1)
+            ->execute();
+
+        return $results->first()['id'];
     }
 
     /**
@@ -65,5 +84,79 @@ class CRM_Wrapi_Actions_Get
             ->execute();
 
         return $results->first();
+    }
+
+    /**
+     * Get Email ID from contact and email type
+     *
+     * @param int $contact_id Contact ID
+     * @param int $loc_type_id Location type id (Home, Main, etc...)
+     * @param bool $check_permissions Should we check permissions (ACLs)?
+     *
+     * @return int|null Email ID if found, null if not found
+     *
+     * @throws API_Exception
+     * @throws UnauthorizedException
+     */
+    public static function emailID(int $contact_id, int $loc_type_id, bool $check_permissions = false): ?int
+    {
+        $results = Email::get($check_permissions)
+            ->addSelect('id')
+            ->addWhere('contact_id', '=', $contact_id)
+            ->addWhere('location_type_id', '=', $loc_type_id)
+            ->setLimit(1)
+            ->execute();
+
+        return $results->first()['id'];
+    }
+
+    /**
+     * Get Phone ID from contact and phone type
+     *
+     * @param int $contact_id Contact ID
+     * @param int $loc_type_id Location type id (Home, Main, etc...)
+     * @param bool $check_permissions Should we check permissions (ACLs)?
+     *
+     * @return int|null Phone ID if found, null if not found
+     *
+     * @throws API_Exception
+     * @throws UnauthorizedException
+     */
+    public static function phoneID(int $contact_id, int $loc_type_id, bool $check_permissions = false): ?int
+    {
+        $results = Phone::get($check_permissions)
+            ->addSelect('id')
+            ->addWhere('contact_id', '=', $contact_id)
+            ->addWhere('location_type_id', '=', $loc_type_id)
+            ->setLimit(1)
+            ->execute();
+
+        return $results->first()['id'];
+    }
+
+    /**
+     * Get Relationship ID from contact and phone type
+     *
+     * @param int $contact_id Contact ID
+     * @param int $other_contact_id Other contact ID (of the relation)
+     * @param int $relationship_type_id Relationship type ID
+     * @param bool $check_permissions Should we check permissions (ACLs)?
+     *
+     * @return int|null Relationship ID if found, null if not found
+     *
+     * @throws API_Exception
+     * @throws UnauthorizedException
+     */
+    public static function relationshipID(int $contact_id, int $other_contact_id, int $relationship_type_id, bool $check_permissions = false): ?int
+    {
+        $results = Relationship::get()
+            ->addSelect('id')
+            ->addWhere('contact_id_a', '=', $contact_id)
+            ->addWhere('contact_id_b', '=', $other_contact_id)
+            ->addWhere('relationship_type_id', '=', $relationship_type_id)
+            ->setLimit(1)
+            ->execute();
+
+        return $results->first()['id'];
     }
 }
